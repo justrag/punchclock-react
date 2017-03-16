7045ca45634954fc578c7ac6a46623b8b3609f28
Just RAG <justrag@gmail.com>
2017-03-16 20:38:48 +0100

    timeFunctions being converted from momoment.js to date-fns

--- src/libs/timeFunctions.js
+++ src/libs/timeFunctions.js
@@ -1,46 +1,66 @@
-export const formatDate = (timestamp, utcOffset=0) => moment(timestamp).utcOffset(utcOffset).format("YYYYMMDD");
+import R from "ramda";
+import format from "date-fans/format";
+import getMonth from "date-fns/get_month";
+import getYear from "date-fns/get_year";
+import startOfWeek from "date-fns/start_of_week";
 
+/// FIXME: WHAT ABOUT UTC OFFSET???
+///check every function that takes utcOffset as param
+
+//export const formatDate = (timestamp, utcOffset=0) => moment(timestamp).utcOffset(utcOffset).format("YYYYMMDD");
+export const formatDate = (timestamp, utcOffset = 0) =>
+  format(new Date(timestamp), "YYYYMMDD");
 //  const formatTime = (timestamp, utcOffset=0) => moment(timestamp).utcOffset(utcOffset).format("HH:mm:ss");
-export const formatTime = (timestamp, utcOffset=0) => moment(timestamp).utcOffset(utcOffset).format("HH:mm");
+export const formatTime = (timestamp, utcOffset = 0) =>
+  format(new Date(timestamp), "HH:mm");
 
-export const splitTime = (timestamp, utcOffset=0) => {
-    let m = moment(timestamp).utcOffset(utcOffset);
-    let formats = {
-      day: "D",
-      month: "MMM",
-      year: "YYYY",
-      hour: "HH",
-      minute: "mm",
-      second: "ss"
-    };
-    return lodash.mapValues(formats, function(val) {
-      return m.format(val);
-    });
+export const splitTime = (timestamp, utcOffset = 0) => {
+  let date = Date(timestamp);
+  let formats = {
+    day: "D",
+    month: "MMM",
+    year: "YYYY",
+    hour: "HH",
+    minute: "mm",
+    second: "ss"
   };
+  return R.map(val => format(date, val), formats);
+};
 
-export const todaysDate = () => moment().format("YYYYMMDD");
-export const hoursToMilliseconds = (ms) => ms * 3600000;
+export const todaysDate = () => format(new Date(), "YYYYMMDD");
+export const hoursToMilliseconds = ms => ms * 3600000;
 
-export const dateTimeStamp = (timestamp = Date.now(), utcOffset=0) => {
-    //let timestamp = Date.now();
-    let date = formatDate(timestamp, utcOffset);
-    let time = formatTime(timestamp, utcOffset);
-    return {
-      timestamp, date, time
-    };
-  }
+export const dateTimeStamp = (timestamp = Date.now(), utcOffset = 0) => {
+  //let timestamp = Date.now();
+  let date = formatDate(timestamp, utcOffset);
+  let time = formatTime(timestamp, utcOffset);
+  return {
+    timestamp,
+    date,
+    time
+  };
+};
 
-export const fromMsToHrsMinsSecs = (duration) => moment.utc(duration).format("HH:mm:ss");
-export const fromMsToHrsMins = (duration) => moment.utc(duration).format("HH:mm");
+export const fromMsToHrsMinsSecs = duration => format(duration, "HH:mm:ss");
+export const fromMsToHrsMins = duration => format(duration, "HH:mm");
 
-export const diffTimestamp = (begin, end, length) => end - begin - hoursToMilliseconds(length);
+export const diffTimestamp = (begin, end, length) =>
+  end - begin - hoursToMilliseconds(length);
 
-export const formatDiffTimestamp = (timestamp) => {
-    let prefix = (timestamp < 0) ? "-" : "+";
-    return prefix + fromMsToHrsMins(Math.abs(timestamp));
-//    return fromMsToHrsMins(Math.abs(timestamp));
-  }
+export const formatDiffTimestamp = timestamp => {
+  let prefix = timestamp < 0 ? "-" : "+";
+  return prefix + fromMsToHrsMins(Math.abs(timestamp));
+};
 
-export const firstDayOfTheWeek = (timestamp) => moment(timestamp).startOf('week').format("YYYYMMDD");
-export const firstDayOfTheMonth = (timestamp) => moment(timestamp).startOf('month').format("YYYYMMDD");
-export const firstDayOfTheYear = (timestamp) => moment(timestamp).startOf('year').format("YYYYMMDD");
+//export const firstDayOfTheWeek = (timestamp) => moment(timestamp).startOf('week').format("YYYYMMDD");
+export const firstDayOfTheWeek = timestamp =>
+  format(startOfWeek(new Date(timestamp)), "YYYYMMDD");
+//export const firstDayOfTheMonth = (timestamp) => moment(timestamp).startOf('month').format("YYYYMMDD");
+export const firstDayOfTheMonth = timestamp =>
+  format(
+    new Date(getYear(new Date(timestamp)), getMonth(new Date(timestamp)), 1),
+    "YYYYMMDD"
+  );
+//export const firstDayOfTheYear = (timestamp) => moment(timestamp).startOf('year').format("YYYYMMDD");
+export const firstDayOfTheYear = timestamp =>
+  format(new Date(getYear(new Date(timestamp)), 0, 1), "YYYYMMDD");
